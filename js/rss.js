10f0e636d08a001c740a224dd699ffdaa5d3f0ad
Sajjad Anwar <me@sajjad.in>
2017-01-24 11:33:40 +0530

    fix missing username and link

--- js/rss.js
+++ js/rss.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
-var team = require('mapbox-data-team').getUsernames();
+var mdt = require('mapbox-data-team');
+var team = mdt.getUsernames();
 var d3 = require('d3-queue');
 var moment = require('moment');
 var he = require('he');
@@ -8,7 +9,7 @@ var baseUrl = "https://www.openstreetmap.org/user/";
 var teamEntries = [];
 var AWS = require("aws-sdk");
 
-var meta = '<div class="clearfix quiet small"><a class="icon account" href="http://www.openstreetmap/user/<%- entry.user %>"><%- entry.author %></a> | <span class="icon time" href=""><%- entry.time %></span></div>';
+var meta = '<div class="clearfix quiet small"><a class="icon account" href="http://www.openstreetmap/user/<%- entry.user %>"><%- entry.user %></a> | <span class="icon time" href=""><%- entry.time %></span></div>';
 
 var title = '<div class="clearfix box round pad2"><div class="clearfix col12"><h2 class="inline fl"><a href="<%- entry.link %>"><%= entry.title %></a></h2></div>'+meta+'</div>';
 
@@ -31,6 +32,9 @@ function fetchEntries(username, callback) {
     }, function(error, response) {
         var json = JSON.parse(response.Payload);
         parser.parseString(json.body, function (err, feed) {
+            feed.feed.entries.forEach(function (f) {
+                f.user = username;
+            });
             Array.prototype.push.apply(teamEntries, feed.feed.entries);
             callback(null);
         });
